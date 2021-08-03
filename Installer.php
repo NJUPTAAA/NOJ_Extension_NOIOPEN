5b26ce9d22abfb04bba549035133368a2856941e
zsgsdesign <zsgsdesign@gmail.com>
2021-08-04 01:19:13 +0800

    complete Installer

--- Installer.php
+++ Installer.php
@@ -1,12 +1,12 @@
 <?php
-namespace App\Babel\Extension\template;//The 'template' should be replaced by the real oj code.
+namespace App\Babel\Extension\noiopen;
 
 use App\Babel\Install\InstallerBase;
 use Exception;
 
 class Installer extends InstallerBase
 {
-    public $ocode="template";//The 'template' should be replaced by the real oj code.
+    public $ocode="noiopen";
 
     public function install()
     {
